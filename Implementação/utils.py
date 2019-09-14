e4742c9f870fcd7d25a3394d7f30c8b23014b421
Isabela Blucher <isabelablucher@gmail.com>
2019-09-14 19:48:07 -0300

    Plot X-Shooter original and divided spectra and change utils function

--- Implementação/utils.py
+++ Implementação/utils.py
@@ -32,13 +32,8 @@ def plot_spectrum(flux, wavelength, filename=None):
 
 
 def find_nearest(array,value):
-    idx = np.searchsorted(array, value, side="left")
-    print(idx - 1, idx)
-    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
-        idx - 1
-    else:
-        return idx
-
+    idx = np.searchsorted(array, value)
+    return(idx - 1, idx)
 
 def fast_dtw_on_stellar_spectra(tel, obs, dist=euclidean):
     distance, path = fastdtw(tel, obs, dist=dist)
